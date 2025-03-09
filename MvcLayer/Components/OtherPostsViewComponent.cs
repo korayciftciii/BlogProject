b5d73a6531cf314388edab8c32d9ac579b1e9e2a
korayciftci <korayciftciii@users.noreply.github.com>
2025-03-09 16:32:10 +0200

    renew Blog card and OtherPosts Component and category card

---
+++ MvcLayer/Components/OtherPostsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Services.Contracts;
+
+namespace MvcLayer.Components
+{
+    public class OtherPostsViewComponent :ViewComponent
+    {
+        private readonly IServiceManager _serviceManager;
+        public OtherPostsViewComponent(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var blogs = await _serviceManager.BlogService.GetAllBlogAsync(false);
+            var randomBlogs = blogs
+                .OrderBy(b => Guid.NewGuid()) // Rastgele sıralama
+                .Take(5) // 5 tanesini al
+                .ToList();
+
+            return View(randomBlogs);
+        }
+    }
+}
